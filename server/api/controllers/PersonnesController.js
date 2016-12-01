671bebb31faa9878b79224e6585a357fcb5f6a2d
root <root@crowdpick>
2016-12-01 23:19:30 +0100

    Personnes controller added

---
+++ server/api/controllers/PersonnesController.js
@@ -0,0 +1,11 @@
+/**
+ * PersonnesController
+ *
+ * @description :: Server-side logic for managing Personnes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+

f73e48b26ec1fece52242d519b140016cc9a11c0
clejacquet <clem.jacquet@gmail.com>
2016-12-02 00:44:16 +0100

    routes

--- server/assets/js/button.js
+++ server/assets/js/button.js
@@ -12,7 +12,7 @@ function errorHandler(){
 $('#sendButton').click(function(){
     var formData = new FormData($('form')[0]);
     $.ajax({
-        url: 'http://app.crowdpick.xyz/personnes/upload',  //Server script to process data
+        url: 'http://app.crowdpick.xyz:1337/personnes/upload',  //Server script to process data
         type: 'POST',
         //crossDomain: true,
         //Ajax events
